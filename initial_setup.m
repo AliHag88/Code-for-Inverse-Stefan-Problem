5294318da5407edc059f4ddce4211e66842149a8
Jonathan Goldfarb <jgoldfar@gmail.com>
2019-02-28 16:26:10 -0500

    Refactor true_solution into a function that returns only other functions, themselves representing the necessary output parameters for the true solution.

--- initial_setup.m
+++ initial_setup.m
@@ -1,3 +1,18 @@
+% TRUE SOLUTION
+[u_true_T, u_true_0, u_true_S, s_star, s_true, u_true] = true_solution(tmesh);
+
+% Use true solution to set measurements
+mu_meas = u_true_S
+w_meas = u_true_T
+
+% INITIAL GUESS
+s_ini = @(t) (s_true(t_final)-1)*t+1;
+a_ini = @(t) ones(size(t))
+
+% alpha (size of step in anti-gradient direction)
+alpha = 10^(-1);
+
+% Old information (unused)
 PHI=1;
 
 g=1;
