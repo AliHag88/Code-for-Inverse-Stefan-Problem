aaee9c2827542ddd6cfa32db7832af8354159028
Jonathan Goldfarb <jgoldfar@gmail.com>
2019-02-28 16:28:15 -0500

    Refactor optimization code into a function, clarify most code with comments, and fixup main loop

--- optimization.m
+++ optimization.m
@@ -1,77 +1,92 @@
-clc; clear all; close all;
+function [J, svals, avals] = optimization(len_xmesh, len_tmesh, tolerance, num_iterations, do_visualization)
+  % optimization: Run ISP example
+  % Input Arguments:
+  %    - len_xmesh: Number of space grid points. Default: 100
+  %    - len_tmesh: Number of time grid points. Default: 100
+  %    - tolerance: Required error for stopping criteria. Default: 1e-5
+  %    - num_iterations: Number of gradient descent steps to take. Default: 200
+  %    - do_visualization: Set to true to emit visualizations during the
+  %      optimization process.
 
-% global t_ini t_fin Nt grad L % For Preconditioning 
+  % Set default arguments
+  if ~exist('len_xmesh', 'var')
+    len_xmesh = 100;
+  end
+  if ~exist('len_tmesh', 'var')
+    len_tmesh = 100;
+  end
+  if ~exist('tolerance', 'var')
+    tolerance = 1e-5
+  end
+  if ~exist('num_iterations', 'var')
+    num_iterations = 200
+  end
+  if ~exist('do_visualization', 'var')
+    do_visualization = false;
+  end
 
+  % Set final moment (eliminate one "magic constant" used in subsequent locations.)
+  t_final = 1;
 
-% Space and Time discritization for both for forward and adjoint problem
-t_ini=0;  % Initial t 
-t_fin=1;  % Final t
-x_ini=0;  % Initial x
-x_fin=1;  % Final x
-Nt=100;   % Number of t grid points
-Nx=100;   % Number of x grid points
-xmesh = linspace(x_ini,x_fin,Nx);  % Space discretization for forward problem
-len_xmesh = length(xmesh); % Number of space grid points
-tmesh = linspace(t_ini,t_fin,Nt); % Time discretization for forward problem
-len_tmesh = length(tmesh); % Number of time grid points
+  % Discretization for both for forward and adjoint problem
+  xmesh = linspace(0,1,len_xmesh);  % Space discretization
+  tmesh = linspace(0,t_final,len_tmesh); % Time discretization
 
-L=1; % Preconditioning 
+  % Initial setup for solver (contains all parameters)
+  % Uses global information to set mu_meas, w_meas, alpha
+  % as well as initial approach s_ini and a_ini
+  %L=1; % Preconditioning 
+  initial_setup;
 
-% time stepsize
-tau=(tmesh(2)-tmesh(1));
+  % Initialize svals and avals
+  svals = s_ini(tmesh);
+  s_der = deriv_est(svals, tmesh);
+  avals = a_ini(tmesh);
 
-% special stepsize
+  % Vector of cost functional values
+  J = zeros(num_iterations,1);
 
-h=(xmesh(2)-xmesh(1));
+  % Counter for number of iterations
+  k = 0;
 
-% INITIAL SETUP FOR TRUE SOLUTION
-initial_setup;
+  % Main Optimization Loop
+  while k < num_iterations
+    k = k + 1
 
-u_T=zeros(length(xmesh),1);
-u_S=zeros(length(tmesh),1);
-u_true_T=zeros(length(tmesh),1);
-% TRUE SOLUTION
-[u_true_T,mu,s_true_vec,s_star,s_true,u_true]=true_solution(xmesh,tmesh);
+    % Calculate solution of forward problem
+    [au_xx_S, u_x_S, u_S, u_T, u] = ...
+      Forward(xmesh, tmesh, svals, avals, u_true_0);
 
-% Initial data of the true solution i.e u_true(x,t)
+    % Calculate solution of adjoint problem
+    [psi_T, psi_t, psi_t_S, psi_S, psi_x_S, psi_x, psi] = ...
+      Adjoint(xmesh, tmesh, svals, avals, u_T, w_meas, s_der, u_S, mu_meas);
 
-uInitial=u_true(1,:);
-
-% INITIAL GUESS 
-
-s_ini = @(t) (s_true(1)-1)*t+1;
-
-% Initialize svals and avals
-
-svals=s_ini(tmesh);
-avals=ones(length(tmesh),1);
-
-% alpha 
-
-alpha=10^(-2);
-
-% Cost Functional 
-
-J=zeros(300,1);
-
-k=0;
-
-% Main Optimization Loop
-
-while abs(svals(end)-s_star)+h*norm(u_T-u_true_T)+tau*norm(u_S-mu)<1000
-   k=k+1  
-   [au_xx_S,svals,s_der,u_x_S,u_S,u_T,avals,sder,u]=Forward(xmesh,tmesh,svals,avals,uInitial); 
-   [psi_T,psi_t,psi_t_S,psi_S,psi_x_S,psi_x,psi]=Adjoint(xmesh,tmesh,svals,avals,u_T,u_true_T,s_der,u_S,mu);
-   
-   grad=grad_s(u,psi,psi_x,tmesh,s_der,svals,u_T,mu,u_x_S,psi_x_S,psi_t_S,psi_t,psi_S,u_S,au_xx_S,s_star,psi_T);
-   % grad=precond()'; % Preconditioning for s(t) gradient
-   svals=svals-alpha*grad;
+    % Take step in antigradient direction
+    svals = svals - alpha * grad_s(u, psi, psi_x, tmesh, s_der, svals, u_T, mu_meas, u_x_S, psi_x_S, psi_t_S, psi_t, psi_S, u_S, au_xx_S, s_star, psi_T);
+    avals = avals - 0 * grad_a(u,psi,tmesh); % Note: avals not updated.
 
 
-   avals=avals-0*grad_a(u,psi,tmesh);
+    % Precondition gradient
+    % grad=precond()'; % Preconditioning for s(t) gradient
 
-   J(k)=abs(svals(end)-s_star)+h*norm(u_T-u_true_T)+tau*norm(u_S-mu);
+    % Update s_der
+    s_der = deriv_est(svals);
 
-   visualization(xmesh,tmesh,svals,avals,u,u_true,len_xmesh, len_tmesh,s_true_vec,s_true,k,J)
-   pause(3)
-end
+    % Calculate functional
+    J(k) = abs(svals(end) - s_star)^2 + ...
+           trapz(xmesh, (u_T - w_meas(xmesh)).^2) + ...
+           trapz(tmesh, (u_S - mu_meas(tmesh)).^2);
+
+    % Do visualization if selected
+    if do_visualization
+      visualization(xmesh, tmesh, svals, avals, u, u_true, s_true, k, J)
+    end
+
+    # Check stopping criteria
+    if J(k) < tolerance
+      break
+    end
+
+  end
+
+end # optimization function
