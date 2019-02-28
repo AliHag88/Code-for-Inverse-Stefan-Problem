dab4f9c1ec1cd1561d43776e8770461aeb67160a
Jonathan Goldfarb <jgoldfar@gmail.com>
2019-02-28 16:25:12 -0500

    Significant (in terms of lines changed) update to Adjoint code - Fixup call to pde solver - Remove unused variables, simplify setup code - Document all input parameters

--- Adjoint.m
+++ Adjoint.m
@@ -1,134 +1,133 @@
-function [psi_T,psi_t,psi_t_S,psi_S,psi_x_S,psi_x,psi]=Adjoint(xmesh,tmesh,svals,avals,u_T,u_true_T,s_der,u_S,mu)
-% Adjoint: Compute values of adjoint for ISP
-% Input Arguments:
-%    - xmesh: Space discretization
-%    - tmesh: Time discretization
-%    - svals: Position of boundary at time grid points
-%    - avals: Diffusion coefficient a(t) at time grid points
-%    - u_T: Vector of values u(x,T;v)
-%    - u_true_T: Vector of measurements u(x,T)=:w(x)
-%    - s_der: Vector of values s'(t)
-%    - u_S: Vector of trace values u(s(t),t;v)
-%    - mu: Vector of values of measurement u(s(t),t)=:mu(t)
+function [psi_T,psi_t,psi_t_S,psi_S,psi_x_S,psi_x,psi] = Adjoint(xmesh,tmesh,svals,avals,u_T,w_meas,s_der,u_S,mu_meas)
+  % Adjoint: Compute values of adjoint for ISP
+  % Input Arguments:
+  %    - xmesh: Space discretization
+  %    - tmesh: Time discretization
+  %    - svals: Position of boundary at time grid points
+  %    - avals: Diffusion coefficient a(t) at time grid points
+  %    - u_T: Vector of values u(x,T;v)
+  %    - w_meas: Function of x, representing measurement u(x,T)=:w(x)
+  %    - s_der: Vector of values s'(t)
+  %    - u_S: Vector of trace values u(s(t),t;v)
+  %    - mu_meas: Function of t, representing measurement u(s(t),t)=:mu(t)
 
-len_xmesh = length(xmesh); % Number of space grid points
-len_tmesh = length(tmesh); % Number of time grid points
+%%%
+%%% Set up solver/parameters
+%%%
 
-h=xmesh(3)-xmesh(2);
-tau=tmesh(3)-tmesh(2);
+  t_final = tmesh(end);
 
-svals=fliplr(svals);
+  % Interpolate s values to form s(t)
+  s_ini = @(t) interp1(tmesh, fliplr(svals), t);
 
-s_der=fliplr(s_der);
+  % Interpolate s' values to form s'(t)
+  sder = @(t) interp1(tmesh, fliplr(s_der), t);
 
-u_S=fliplr(u_S);
+  % Interpolate a values to form a(t)
+  af = @(t) interp1(tmesh, fliplr(avals), t);
 
-mu=fliplr(mu);
+  uT = @(x) interp1(xmesh, u_T, x);
 
-% Interpolate s values to form s(t)
-s_ini = @(t) interp1(tmesh, svals, t);
+  uS = @(t) interp1(tmesh, fliplr(u_S), t);
 
-% Interpolate s' values to form s'(t)
-sder = @(t) interp1(tmesh, s_der, t);
-
-% Interpolate a values to form a(t)
-af = @(t) interp1(tmesh, avals, t);
-
-uT=@(x) interp1(xmesh,u_T,x);
-
-uTrueT=@(x) interp1(xmesh,u_true_T,x);
-
-uS=@(t) interp1(tmesh, u_S, t);
-
-mU=@(t) interp1(tmesh, mu, t);
+  mu = @(t) mu_meas(t_final - t);
 
 %%%
 %%% Calculate solution on rectangular domain
 %%%
-psi = pdeSolver(xmesh, tmesh, af, s_ini, sder, uT, uTrueT, uS, mU);
+psi = pdeSolver(xmesh, tmesh, af, s_ini, sder, uT, w_meas, uS, mu);
 
 %%%
 %%% Calculate values derived from psi
 %%%
+
+% Number of space grid points
+len_xmesh = length(xmesh);
+
+% spatial stepsize
+h = xmesh(3)-xmesh(2);
+
+% time stepsize
+tau = tmesh(2)-tmesh(1);
+
 % psi_x (x,t) matrix of derivatives of psi(x,t)
-psi_x=zeros(len_tmesh, len_xmesh);
+psi_x = zeros(size(psi));
 psi_x(:, 1:end-1) = psi(:, 2:end) - psi(:, 1:end-1);
 psi_x(:, end) = psi(:, end) - psi(:, end-1);
 % Scale all values
 psi_x = psi_x / h;
 
-for i=1:len_xmesh
-psi_x(:,i)=psi_x(:,i)./svals';
+for i = 1:len_xmesh
+  psi_x(:,i) = psi_x(:,i) ./ svals(:);
 end
 
 % Values of psi_x (x,t) at s(t)
-psi_x_S=psi_x(:, end);
+psi_x_S = psi_x(:, end);
 
 
 % psi_t (x,t) matrix of derivatives of psi(x,t) w.r.t t
-psi_t=zeros(length(tmesh),length(xmesh));
+psi_t=zeros(size(psi));
 psi_t(1:end-1, :) = psi(2:end,:) - psi(1:end-1,:);
 psi_t(end, :) = psi(end, :) - psi(end-1,:);
 % Scale all values
 psi_t = psi_t / tau;
 
-for i=1:len_xmesh
-    psi_t(:,i)=psi_t(:,i)-psi_x(:,i).*s_der;
+for i = 1:len_xmesh
+    psi_t(:,i) = psi_t(:,i) - psi_x(:,i) .* s_der(:);
 end
 
 % Values of psi_t (x,t) at s(t)
 psi_t_S = psi_t(:, end);
 
 % Values of psi (x,t) at s(t)
-psi_S=psi(:, end);
+psi_S = psi(:, end);
 
 % Values Of psi(x,t) at t=T
-psi_T=psi(end, :);
-
+psi_T = psi(end, :);
 end
 % End Main Function
 
 
 % Begin Subfunctions
-function psi = pdeSolver(xmesh, tmesh, af, sf,sder,uT,uTrueT,uS,mU)
-m=0; % Symmetry of the problem. 0=slab (rectangular)
+function psi = pdeSolver(xmesh, tmesh, af, sf, sder, uT, uTrueT, uS, mU)
+  m = 0; % Symmetry of the problem. 0=slab (rectangular)
 
-% PDE to be solved is
-%    c u_t = x^{-m} (x^m f(x,t,u, DuDx))_x + s
-% The output values below give the corresponding value at each point (x, t, u, u_x)
-    function [c, f, s] = pde(x,t,~,DuDx)
-        c = -sf(t)^2;
-        f = -af(t)*DuDx;
-        s =sf(t)*sder(t)*x*DuDx;
-    end
+  % PDE to be solved is
+  %    c u_t = x^{-m} (x^m f(x,t,u, DuDx))_x + s
+  % The output values below give the corresponding value at each point (x, t, u, u_x)
+  pde = @(x,t,u, DuDx) ...
+         deal(...
+          -sf(t)^2, ... #c
+          -af(t)*DuDx, ... #f
+          sf(t)*sder(t)*x*DuDx ... #s
+         );
 
-% Initial condition u(x,t_0)
-    function psi = ic(x)
-        psi = 2*(uT(x)-uTrueT(x));
-    end
+  % Initial condition u(x,t_0)
+  ic = @(x) 2*(uT(x)-uTrueT(x));
 
-% Boundary condition at x=xl and x=xr.
-% Suffix l corresponds to left-hand side of domain,
-% Suffix r corresponds to right-hand side of domain.
-% Boundary condition takes the form
-%    p(x,t,u) + q(x,t) f(x,t,u,u_x) = 0
-% where f is defined below.
-% In particular, the inputs correspond to (x,u) at x=xl and (x,u) at x=xr
-% and the time t, and the outputs correspond to (p,q) at x=xl and (p,q) at
-% x=xr
-    function [pl, ql, pr, qr] = bc(~, ul, ~, ~, t)
-        pl =0;
-        ql = 1;
-        pr =sder(t)*sf(t)*ul+2*sf(t)*(uS(t)-mU(t));
-        qr = -1;
-    end
+  % Boundary condition at x=xl and x=xr.
+  % Suffix l corresponds to left-hand side of domain,
+  % Suffix r corresponds to right-hand side of domain.
+  % Boundary condition takes the form
+  %    p(x,t,u) + q(x,t) f(x,t,u,u_x) = 0
+  % where f is defined below.
+  % In particular, the inputs correspond to (x,u) at x=xl and (x,u) at x=xr
+  % and the time t, and the outputs correspond to (p,q) at x=xl and (p,q) at
+  % x=xr
+  bc = @(xl, ul, xr, ur, t) ...
+        deal(...
+              0, ... # pl
+              1, ... # ql
+              sder(t)*sf(t)*ul+2*sf(t)*(uS(t)-mU(t)), ... # pr
+              -1 ... # qr
+            );
 
-% Calculate solution evaluated at each point of xmesh and tmesh.
-sol = pdepe(m, @pde, @ic, @bc, xmesh, tmesh);
+  % Calculate solution evaluated at each point of xmesh and tmesh.
+  sol = pdepe(m, pde, ic, bc, xmesh, tmesh);
 
-% Extract the first solution component as u.
-% Note: This solution is defined on tmesh * xmesh.
-% TODO: Transform to function u(x,t) within this routine.
-psi = flipud(sol(:, :, 1));
+  % Extract the first solution component as psi.
+  % Note: This solution is defined on tmesh * xmesh.
+  % TODO: Transform to function psi(x,t) within this routine.
+  psi = flipud(sol(:, :, 1));
 end
 % End Subfunctions
--- test_Adjoint.m
+++ test_Adjoint.m
@@ -1,4 +1,4 @@
-function errorOut = test_Forward(len_xmesh, len_tmesh)
+function psiErrorOut = test_Adjoint(len_xmesh, len_tmesh)
 % len_xmesh: Number of space grid points
 % len_tmesh: Number of time grid points
 
@@ -7,37 +7,32 @@ tmesh = linspace(0,1,len_tmesh); % Time discretization for forward problem
 
 s_new = @(t) (3*exp(-t.^2)-1)/2;
 
-[X, T] = meshgrid(xmesh, tmesh);
-
-u_true=@(x,t) x.^2*t+x*t;
-u_true_0 = @(x) u_true(x, 0);
-
 boundary_values = s_new(tmesh);
+s_der = deriv_est(boundary_values, tmesh);
+
+% Set up problem with vanishing boundary data
+u_T = ones(size(xmesh));
+w_meas = @(x) ones(size(x));
+
+u_S = -ones(size(tmesh));
+mu_meas = @(t) -ones(size(t));
+
 avals = ones(size(tmesh));
 
-[au_xx_S,svals,s_der,u_x_S,u_S,u_T,avals,sder,u] = Forward(xmesh,tmesh,boundary_values,avals,u_true_0);
+% Run solver
+[psi_T, psi_t, psi_t_S, psi_S, psi_x_S, psi_x, psi] = ...
+    Adjoint(xmesh, tmesh, boundary_values, avals, u_T, w_meas, s_der, u_S, mu_meas);
 
-% u:[0, max(s)] x [0, 1] \to R is defined by interpolation.
-u_interp = @(x,t) interp2(X, T, u, x/s_new(t), t, 'linear', NaN);
+% Check size of outputs
+assert (all(size(psi) == [len_tmesh, len_xmesh]))
+assert (all(size(psi_t) == [len_tmesh, len_xmesh]))
+assert (all(size(psi_x) == [len_tmesh, len_xmesh]))
 
-x_new = linspace(0, max(boundary_values), len_xmesh);
+assert (length(psi_T) == len_xmesh)
+assert (length(psi_t_S) == len_tmesh)
+assert (length(psi_x_S) == len_tmesh)
+assert (length(psi_S) == len_tmesh)
 
-diff_visual = zeros(len_tmesh, len_xmesh);
-for i = 1:len_tmesh
-    for j = 1:len_xmesh
-        if x_new(j) > boundary_values(i)
-            break
-        end
-        diff_visual(i, j) = u_true(x_new(j), tmesh(i)) - u_interp(x_new(j), tmesh(i));
-    end
-end
-
-assert all(size(u) == [len_tmesh, len_xmesh])
-assert (length(au_xx_S) == len_tmesh)
-assert (length(u_S) == len_tmesh)
-assert (length(u_x_S) == len_tmesh)
-assert (length(u_T) == len_xmesh)
-
-errorOut = norm(diff_visual, 'fro')*(x_new(2)-x_new(1))*(tmesh(2)-tmesh(1));
+psiErrorOut = norm(psi, 'fro')*(xmesh(2)-xmesh(1))*(tmesh(2)-tmesh(1));
 
 end
