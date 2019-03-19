5b6cb0f95b982fb3b65095477e6a601f92801bb5
Jonathan Goldfarb <jgoldfar@gmail.com>
2019-03-19 10:27:45 -0400

    Clarify that the output from initial_setup is the maximum step size, and what the meaning of the declinations from the analytic solution being printed are.

--- initial_setup.m
+++ initial_setup.m
@@ -1,4 +1,4 @@
-function [step_size, u_true_0, mu_meas, w_meas, g, s_star, s_ini, a_ini] = initial_setup(tmesh, xmesh)
+function [max_step_size, u_true_0, mu_meas, w_meas, g, s_star, s_ini, a_ini] = initial_setup(tmesh, xmesh)
 % The true solution values are used only for the measurements s_star
 [u_true_T, u_true_0, u_true_S, g, s_star, s_true, ~, a_true] = true_solution(tmesh);
 
@@ -10,8 +10,8 @@ s0 = s_true(t_initial);
 % Extract final time value as t_final
 t_final = tmesh(end);
 
-% step_size (size of step in anti-gradient direction)
-step_size = 1/300;
+% maximum step size in anti-gradient direction
+max_step_size = 1;
 
 % Use true solution to set measurements
 %mu_meas = u_true_S;
@@ -19,8 +19,8 @@ step_size = 1/300;
 
 % Initial Guess
 %s_ini = @(t) (s_star - s0)/(t_final - t_initial) * (t - t_initial) + s0;
-s_ini = @(t) s_true(t);
-a_ini = a_true;
+s_ini = @(t) s_true(t) + 0.1;
+a_ini = @(t) a_true(t);
 
 
 % Synthetic measurements
@@ -31,9 +31,9 @@ avals_ = a_true(tmesh);
 
 % Interpolate a values to form u_true_S
 mu_meas = @(t) interp1(tmesh, u_S_, t);
-%mu_meas(tmesh) - u_true_S(tmesh)
+disp(['S trace declination: ' num2str(norm(mu_meas(tmesh) - u_true_S(tmesh)))]);
 
 % Interpolate w values to form u_true_T
 w_meas = @(x) interp1(xmesh, u_T_, x);
-%w_meas(xmesh) - u_true_T(xmesh * svals_(end))
+disp(['T trace declination: ' num2str(norm(w_meas(xmesh) - u_true_T(xmesh * svals_(end))))]);
 end
