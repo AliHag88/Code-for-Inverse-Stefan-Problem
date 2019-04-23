d3f5bbdfb3235489e1220e78d7275d8d94059dc8
Jonathan Goldfarb <jgoldfar@gmail.com>
2019-04-23 10:43:46 -0400

    Document grad_a

--- grad_a.m
+++ grad_a.m
@@ -3,8 +3,10 @@ function [grad]=grad_a(u,psi,tmesh,xmesh,svals)
   grad = zeros(size(tmesh));
   for ti = 1:length(tmesh)
     xmesh_curr = xmesh*svals(ti);
-    [u_trace, u_x_trace] = pdeval(0, xmesh_curr, u(ti, :), xmesh_curr);
-    [psi_trace, psi_x_trace] = pdeval(0, xmesh_curr, psi(ti, :), xmesh_curr);
+    % First argument is u trace (ignored)
+    [~, u_x_trace] = pdeval(0, xmesh_curr, u(ti, :), xmesh_curr);
+    % Second argument is psi_x trace (ignored)
+    [psi_trace, ~] = pdeval(0, xmesh_curr, psi(ti, :), xmesh_curr);
     grad(ti) = -2*u_x_trace(1)*psi_trace(1) - trapz(xmesh_curr, u_x_trace .* psi_trace);
   end
 end
