0218f8e9025d3c22fb0cb8e676d8d6a7ff1ec3e7
Jonathan Goldfarb <jgoldfar@gmail.com>
2019-05-02 08:50:04 -0400

    Make gradient quality plot more robust: - Allow/ignore errors during optimization process - Output progress to file incrementally rather than at once - Add documentation
    (cherry picked from commit b1eea948a8f5bbff9d44981bed6a61168b3a0378)

--- plot_gradientQuality.m
+++ plot_gradientQuality.m
@@ -1,101 +1,109 @@
 function [] = plot_gradientQuality(generate_data, generate_plots, Nx, Nt, n_fd_epsilon)
+  % Call same setup code as in optimization script
+  % (any duplicated variables will be overwritten later.
+  optimization_parameter_defaults;
 
-% Set default values of the input arguments
-if ~exist('generate_data', 'var')
+  % Set default values of the input arguments
+  if ~exist('generate_data', 'var')
     generate_data = true;
-end
-if ~exist('generate_plots', 'var')
+  end
+  if ~exist('generate_plots', 'var')
     generate_plots = false;
-end
-if ~exist('Nx', 'var')
-    Nx = 40;
-end
-if ~exist('Nt', 'var')
-    Nt = Nx^2;
-end
-% Number of iterations to test
-if ~exist('n_fd_epsilon', 'var')
+  end
+                                % Number of iterations to test
+  if ~exist('n_fd_epsilon', 'var')
     n_fd_epsilon = 20;
-end
+  end
 
-% Fixed parameters for example
-initial_data_parameter_s = 0.6;
-initial_data_parameter_a = 0.6;
+  % Compute filename corresponding to this example
+  output_filename = sprintf('kappa_Nx%d_Nt%d.csv', Nx, Nt);
+  plot_filename = sprintf('kappa_Nx%d_Nt%d.pdf', Nx, Nt);
 
-regularization_s = 0.5;
-regularization_a = 0.22;
+  % Headers and file format for output
+  output_headers = ['fd_epsilon,kappa_s,kappa_s_precond,kappa_a,kappa_a_precond,' ...
+                      'logkappa_s,logkappa_s_precond,logkappa_a,logkappa_a_precond\n' ];
 
-% Compute filename corresponding to this example
-kappa_value_filename = sprintf('kappa_Nx%d_Nt%d.csv', Nx, Nt);
+  % Output format and size of each row
+  output_format = '%.16f,';
+  output_size = [1, 9];
 
-% Headers and file format for output
-output_headers = ['fd_epsilon,kappa_s,kappa_s_precond,kappa_a,kappa_a_precond,' ...
-                  'logkappa_s,logkappa_s_precond,logkappa_a,logkappa_a_precond\n' ];
+  % Generate data
+  if generate_data
 
-output_format = '%.16f,';
-output_size = [1, 9];
+    % Initialize storage
+    fd_epsilon_values = linspace(eps(0), 1e-1, n_fd_epsilon)';
+    kappa_s = zeros(size(fd_epsilon_values))*NaN;
+    kappa_s_precond = zeros(size(fd_epsilon_values))*NaN;
+    kappa_a = zeros(size(fd_epsilon_values))*NaN;
+    kappa_a_precond = zeros(size(fd_epsilon_values))*NaN;
 
-% Generate data
-if generate_data
+    % Open file to export data in CSV format
+    fileID = fopen(output_filename, 'w');
 
-% Initialize storage
-fd_epsilon_values = linspace(eps(0), 1e-1, n_fd_epsilon)';
-kappa_s = zeros(size(fd_epsilon_values))*NaN;
-kappa_s_precond = zeros(size(fd_epsilon_values))*NaN;
-kappa_a = zeros(size(fd_epsilon_values))*NaN;
-kappa_a_precond = zeros(size(fd_epsilon_values))*NaN;
+    % Add header
+    fprintf(fileID, output_headers);
 
-% Calculate kappa values
-for i = 1:length(fd_epsilon_values)
-    fd_epsilon_curr = fd_epsilon_values(i);
-    [kappa_s(i) kappa_s_precond(i) kappa_a(i) kappa_a_precond(i)] = ...
+    % Calculate kappa values
+    for i = 1:length(fd_epsilon_values)
+      fd_epsilon_curr = fd_epsilon_values(i);
+      try
+        [kappa_s(i), kappa_s_precond(i), kappa_a(i), kappa_a_precond(i)] = ...
         gradientQuality(Nx, Nt, fd_epsilon_curr, ...
                         initial_data_parameter_s, initial_data_parameter_a, ...
                         regularization_s, regularization_a);
-end
+      catch ME # Fail with grace (by ignoring the issue.)
+        disp(ME)
+      end
 
-% Export data in CSV format
-fileID = fopen(kappa_value_filename, 'w');
-
-fprintf(fileID, output_headers);
-for i = 1:length(fd_epsilon_values)
-    fprintf(fileID, [repmat(output_format, output_size) '\n'], ...
-                    fd_epsilon_values(i), ...
-                    kappa_s(i), kappa_s_precond(i), ...
-                    kappa_a(i), kappa_a_precond(i), ...
-                    log10(abs(kappa_s(i) - 1)), log10(abs(kappa_s_precond(i) - 1)), ...
-                    log10(abs(kappa_a(i) - 1)), log10(abs(kappa_a_precond(i) - 1)) ...
-            );
-end % For loop
-fclose(fileID);
-end % Data generation
-
-
-% Generate plots
-if generate_plots
-    if ~exist('kappa_s', 'var') && isfile(kappa_value_filename)
-        fileID = fopen(kappa_value_filename, 'r');
-        fgetl(fileID); % Skip header
-        % Initialize storage
-        fd_epsilon_values = [];
-        kappa_s = [];
-        kappa_s_precond = [];
-        kappa_a = [];
-        kappa_a_precond = [];
-        % Read all lines from file
-        while true
-            tline = fgetl(fileID);
-            if tline == -1 % Documented as EOL marker
-                break
-            end
-            values_tmp = sscanf(tline, '%f,', output_size);
-            fd_epsilon_values(end + 1) = values_tmp(1);
-            kappa_s(end + 1) = values_tmp(2);
-            kappa_s_precond(end + 1) = values_tmp(3);
-            kappa_a(end + 1) = values_tmp(4);
-            kappa_a_precond(end + 1) = values_tmp(5);
-        end
+      % Output to file
+      fprintf(fileID, [repmat(output_format, output_size) '\n'], ...
+              fd_epsilon_values(i), ...
+              kappa_s(i), kappa_s_precond(i), ...
+              kappa_a(i), kappa_a_precond(i), ...
+              log10(abs(kappa_s(i) - 1)), log10(abs(kappa_s_precond(i) - 1)), ...
+              log10(abs(kappa_a(i) - 1)), log10(abs(kappa_a_precond(i) - 1)) ...
+             );
     end
+    % Close data file
+    fclose(fileID);
+
+  end % if generate_data
+
+
+  % Generate plots
+  if generate_plots
+  % If data already exists in this workspace, use it.
+  % Otherwise, read it from the file.
+    if ~exist('kappa_s', 'var') && isfile(output_filename)
+      fileID = fopen(output_filename, 'r');
+      fgetl(fileID); % Skip header
+
+      % Initialize storage
+      fd_epsilon_values = [];
+      kappa_s = [];
+      kappa_s_precond = [];
+      kappa_a = [];
+      kappa_a_precond = [];
+
+      % Read all lines from file
+      while true
+        tline = fgetl(fileID);
+        if tline == -1 % Documented as EOL marker from fgetl
+          break
+        end
+
+        % Parse data from current line
+        values_tmp = sscanf(tline, '%f,', output_size);
+
+        % Extract values from temp array
+        fd_epsilon_values(end + 1) = values_tmp(1);
+        kappa_s(end + 1) = values_tmp(2);
+        kappa_s_precond(end + 1) = values_tmp(3);
+        kappa_a(end + 1) = values_tmp(4);
+        kappa_a_precond(end + 1) = values_tmp(5);
+      end % while loop
+    end % if variables don't exist
+
     %% Plots for s(t)
     % kappa_s
     subplot(2,2,1);
@@ -127,10 +135,11 @@ if generate_plots
     title(sprintf('\\kappa_a, Precond:True', Nx, Nt));
 
     if exist('sgtitle')
-        sgtitle(sprintf('Nx=%d,Nt=%d', Nx, Nt));
+      sgtitle(sprintf('Nx=%d,Nt=%d', Nx, Nt));
     else
-        title(sprintf('Nx=%d,Nt=%d', Nx, Nt));
+      title(sprintf('Nx=%d,Nt=%d', Nx, Nt));
     end
-end
+    saveas(gcf, plot_filename, 'pdf');
+  end % if generate_plots
 
 end % Function
